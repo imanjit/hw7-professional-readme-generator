ce302bfbb6b4b48ee8b9766a40cc6495180c83f2
Iman Bains <imanjitbains@yahoo.com>
2021-06-07 12:36:40 -0700

    Added questions

--- index.js
+++ index.js
@@ -1,7 +1,58 @@
 // TODO: Include packages needed for this application
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: "input",
+        name: "github",
+        message: "Github username?",
+    },
+    {
+        type: "input",
+        name: "email",
+        message: "Email address?",
+    },
+    {
+        type: "input",
+        name: "title",
+        message: "Project name?",
+    },
+    {
+        type: "input",
+        name: "description",
+        message: "Project description?",
+    },
+    {
+        type: "input",
+        name: "installation",
+        message: "Command to install dependencies?",
+        default: "npm i",
+    },
+    {
+        type: "input",
+        name: "test",
+        message: "Command to run tests?",
+        default: "npm test",
+    },
+    {
+        type: "list",
+        name: "license",
+        message: "Project license?",
+        choices: [
+            "APACHE 2.0",
+            "Boost", 
+            "BSD 2",
+            "BSD 3",
+            "Creative Commons Zero",
+            "Eclipse",
+            "GNU Affero",
+            "GNU General 2",
+            "GNU General 3",
+            "GNU Lesser",
+            "MIT",
+        ]
+    },
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
